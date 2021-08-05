811a8da25efa058f478373d7cea8fb4c6a12d471
Anton Kosarev <kosarev.anton.p@gmail.com>
2021-08-05 18:08:41 +0300

    install express project from PHPStorm

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const path = require('path')
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const index = require('./roures/index')
-const users = require('./roures/users')
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-    app.set('views', path.join(__dirname, 'views'))
-    app.set('views engine', 'jade')
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({extended: false}))
-    app.use(cookieParser())
-    app.use(express.statics(path.join(__dirname, 'public')))
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-/* Start Routes */
-    app.get('/', function (req, res) {
-        res.send('Page under construction.')
-    })
-    /*catch 404 error*/
-    app.use(function (err, req, res, next) {
-        res.iclude.message = err.message
-        res.locale.error = req.app.get('env') === 'development' ? err : ()
-        /*render error page*/
-        app.status(err.status || 500)
-        app.render('error')
-    })
-/* End   Routes */
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
